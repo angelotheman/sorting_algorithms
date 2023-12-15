876be6b980a3ea2509b2537b80cc14f602cb7958
angelotheman <ktwum721@gmail.com>
2023-12-15 11:16:42 +0000

    Updated with swap function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,6 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
-	int tmp;
 
 	for (i = 0; i < size; i++)
 	{
@@ -23,9 +22,7 @@ void selection_sort(int *array, size_t size)
 		}
 		if (i != min)
 		{
-			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
+			swap(&array[i], &array[min]);
 			print_array(array, size);
 		}
 	}
