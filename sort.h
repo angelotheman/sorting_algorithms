6cb52d4f8038e5b0f5be62b83cc953c1b45e839f
angelotheman <ktwum721@gmail.com>
2023-12-17 14:51:21 +0000

    Done deal

--- sort.h
+++ sort.h
@@ -27,8 +27,10 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
 /* ---- HELPER FUNCTIONS ---- */
 void swap(int *var1, int *var2);
+size_t partition(int *array, size_t low, size_t high);
 
 #endif /* SORT_H */
