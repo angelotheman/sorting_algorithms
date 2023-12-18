2f83a0603929565464c0450a21fe90a4b6d19f31
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-18 23:20:37 +0100

    code for radix sort algo completed

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -17,8 +17,7 @@ void radix_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 	max = get_max(array, size);
-	printf("max => %d\n", max);
-	for (pos = 1; pos / max > 0; pos *= 10)
+	for (pos = 1; max / pos > 0; pos *= 10)
 	{
 		count_sort(array, size, pos);
 		print_array(array, size);
@@ -57,33 +56,25 @@ int get_max(int *array, size_t size)
 
 void count_sort(int *array, size_t size, int pos)
 {
-	size_t i, c_size;
-	int *count, *barray;
+	int count[10] = {0}, *barray;
+	int i;
 
-	c_size = 10;
-	count = malloc(sizeof(int) * c_size);
-	if (count == NULL)
-		return;
-	for (i = 0; i < c_size; i++)
-		count[i] = 0;
 	barray = malloc(sizeof(int) * size);
 	if (barray == NULL)
-	{
-		free(count);
 		return;
-	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 		count[(array[i] / pos) % 10]++;
 
 	print_array(count, 10);
-	for (i = 1; i < c_size; i++)
+	for (i = 1; i < 10; i++)
 		count[i] = count[i] + count[i - 1];
-	for (i = 0; i < size; i++)
+	print_array(count, 10);
+	for (i = size - 1; i >= 0; i--)
 		barray[--count[(array[i] / pos) % 10]] = array[i];
-	for (i = 0; i < size; i++)
+	print_array(barray, size);
+	for (i = 0; i < (int)size; i++)
 		array[i] = barray[i];
 
-	free(count);
 	free(barray);
 }
 
