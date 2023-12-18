ffed285ca32a2de72a07ba3195057a8c40d224dc
angelotheman <ktwum721@gmail.com>
2023-12-18 14:53:00 +0000

    Revert to original

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -23,7 +23,7 @@ void shell_sort(int *array, size_t size)
 		{
 			for (i = j - gap; i >= 0; i -= gap)
 			{
-				if (array[i + gap] < array[i])
+				if (array[i + gap] > array[i])
 					break;
 				swap(&array[i + gap], &array[i]);
 			}
