f31d0166d5b661f5d44e5555062dd13d2e944bc7
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-19 18:54:25 +0100

    fixed betty errors

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -12,26 +12,27 @@ void heapify(int *array, size_t o_size, size_t size, size_t n);
 
 void heap_sort(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    for (i = size / 2; i >= 1; i--)
-        heapify(array, size, size, i);
+	for (i = size / 2; i >= 1; i--)
+		heapify(array, size, size, i);
 
-    for (i = size; i >= 1; i--)
-    {
-        if (&array[i - 1] != &array[0])
-        {
-            swap(&array[i - 1], &array[0]);
-            print_array(array, size);
-        }
-        heapify(array, size, i, 1);
-    }
+	for (i = size; i >= 1; i--)
+	{
+		if (&array[i - 1] != &array[0])
+		{
+			swap(&array[i - 1], &array[0]);
+			print_array(array, size);
+		}
+		heapify(array, size, i, 1);
+	}
 }
 
 
 /**
  * heapify - sorts elements in a heap tree section
  * @array: array
+ * @o_size: original size of array
  * @size: size of array
  * @n: position of element in tree
  * Return: void
@@ -39,24 +40,24 @@ void heap_sort(int *array, size_t size)
 
 void heapify(int *array, size_t o_size, size_t size, size_t n)
 {
-    size_t largest_index, left_child, right_child;
+	size_t largest_index, left_child, right_child;
 
-    largest_index = n;
-    left_child = 2 * n;
-    right_child = 2 * n + 1;
+	largest_index = n;
+	left_child = 2 * n;
+	right_child = 2 * n + 1;
 
-    if (left_child < size && array[largest_index - 1] < array[left_child - 1])
-        largest_index = left_child;
+	if (left_child < size && array[largest_index - 1] < array[left_child - 1])
+		largest_index = left_child;
 
-    if (right_child < size && array[largest_index - 1] < array[right_child - 1])
-        largest_index = right_child;
+	if (right_child < size && array[largest_index - 1] < array[right_child - 1])
+		largest_index = right_child;
 
-    if (largest_index != n)
-    {
-        swap(&array[largest_index - 1], &array[n - 1]);
-        print_array(array, o_size);
-        heapify(array, o_size, size, largest_index);
-    }
+	if (largest_index != n)
+	{
+		swap(&array[largest_index - 1], &array[n - 1]);
+		print_array(array, o_size);
+		heapify(array, o_size, size, largest_index);
+	}
 }
 
 /**
@@ -68,9 +69,9 @@ void heapify(int *array, size_t o_size, size_t size, size_t n)
 
 void swap(int *var1, int *var2)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *var1;
-    *var1 = *var2;
-    *var2 = tmp;
+	tmp = *var1;
+	*var1 = *var2;
+	*var2 = tmp;
 }
